156f8a9facb039a7b81015f1f8a061d07746a5aa
Julian Cromarty <julian.cromarty@gmail.com>
2016-09-20 21:10:22 +0100

    Create basic project skeleton

---
+++ src/Mimic.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Mimic.cpp
+ Author      : Julian Cromarty
+ Version     :
+ Copyright   : Copyright (c)2016, Julian Cromarty
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
